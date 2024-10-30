a934666893570b28bd9df69e387bf3efad8d28fb
juanchoalcoba <juanchoalcoba@gmail.com>
2024-10-30 19:40:07 -0300

    update

--- MediLink/src/layout/Layout.jsx
+++ MediLink/src/layout/Layout.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import Footer from "../components/Footer";
 import Routers from "../routes/Routers";
-import Lenis from '@studio-freight/lenis'; // Importar Lenis
+import Lenis from '@studio-freight/lenis'; 
 
 const Layout = () => {
 
   useEffect(() => {
-    // Inicializar Lenis
+    
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Curva de suavidad
-      direction: 'vertical', // Scroll vertical
+      direction: 'vertical', 
       gestureDirection: 'vertical', 
       smooth: true,
       mouseMultiplier: 1,
@@ -19,20 +19,17 @@ const Layout = () => {
       infinite: false,
     });
 
-    // Capturar el evento de scroll
     lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
       console.log({ scroll, limit, velocity, direction, progress });
     });
 
-    // Función para animar el scroll
-    function raf(time) {
+   function raf(time) {
       lenis.raf(time);
       requestAnimationFrame(raf);
     }
 
     requestAnimationFrame(raf);
 
-    // Cleanup al desmontar el componente
     return () => {
       lenis.destroy();
     };
